97e86fa4c6d8a798bb969b37b024a14fb0289536
Steve-Steveson <mr.steve.steveson@gmail.com>
2026-05-24 21:59:49 -0400

    spoof mixin

---
+++ src/main/java/net/steveson/createtrimmable/mixin/AArmorTrimMixin.java
@@ -0,0 +1,53 @@
+package net.steveson.createtrimmable.mixin;
+
+import com.simibubi.create.Create;
+import com.simibubi.create.content.equipment.armor.AllArmorMaterials;
+import net.minecraft.Util;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.armortrim.ArmorTrim;
+import net.minecraft.world.item.armortrim.TrimPattern;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.BiFunction;
+
+@Mixin(ArmorTrim.class)
+public abstract class AArmorTrimMixin {
+
+    @Shadow
+    @Final
+    private Holder<TrimPattern> pattern;
+
+    @Shadow
+    protected abstract String getColorPaletteSuffix(ArmorMaterial pArmorMaterial);
+
+    @Unique
+    private final BiFunction<Boolean, ArmorMaterial, ResourceLocation> create$textureCardboardTwo = Util.memoize((inner, material) -> {
+        String assetPath = pattern.value().assetId().getPath();
+        String colorSuffix = getColorPaletteSuffix(material);
+//        System.out.println("zq I AM THE PRINT LINE; BEHOLD MY LINE OF PRINTING!");
+//        System.out.println(colorSuffix);
+        return Create.asResource("trims/models/armor/card_" + assetPath + (inner ? "_leggings_" : "_") + colorSuffix);
+    });
+
+    @Inject(method = "innerTexture", at = @At("HEAD"), cancellable = true)
+    private void create$swapTexturesForCardboardTrimsInner(ArmorMaterial pArmorMaterial, CallbackInfoReturnable<ResourceLocation> cir) {
+        if (pArmorMaterial == AllArmorMaterials.CARDBOARD) {
+            cir.setReturnValue(create$textureCardboardTwo.apply(true, pArmorMaterial));
+        }
+    }
+
+    @Inject(method = "outerTexture", at = @At("HEAD"), cancellable = true)
+    private void create$swapTexturesForCardboardTrimsOuter(ArmorMaterial pArmorMaterial, CallbackInfoReturnable<ResourceLocation> cir) {
+        if (pArmorMaterial == AllArmorMaterials.CARDBOARD) {
+            cir.setReturnValue(create$textureCardboardTwo.apply(false, pArmorMaterial));
+        }
+    }
+}
